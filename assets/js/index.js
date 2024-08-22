6afd89d96d61cb14e2ef5450db2bda479fb87f37
BrainX Shopify <brainxshopify@BrainXs-MacBook-Pro.local>
2024-08-22 12:08:51 +0500

    js changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,64 @@
-function ageCalculate() {
-    let day = parseInt(document.getElementById('day').value);
-    let month = parseInt(document.getElementById('month').value);
-    let year = parseInt(document.getElementById('year').value);
+document.addEventListener('DOMContentLoaded', function () {
+    const btnGo = document.getElementById('btn-go');
 
-    console.log(day);
-    console.log(month);
-    console.log(year);
+    btnGo.addEventListener('click', function() {
+        let day = parseInt(document.getElementById('day').value);
+        let month = parseInt(document.getElementById('month').value);
+        let year = parseInt(document.getElementById('year').value);
 
-    const currentDate = new Date();
-    let currentDay = currentDate.getDate();
-    let currentMonth = currentDate.getMonth() + 1; 
-    let currentYear = currentDate.getFullYear();
+        const currentDate = new Date();
+        let currentDay = currentDate.getDate();
+        let currentMonth = currentDate.getMonth() + 1; 
+        let currentYear = currentDate.getFullYear();
 
-    console.log(currentDay);
-    console.log(currentMonth);
-    console.log(currentYear);
+        let age = currentYear - year;
 
-    let totalYears = currentYear - year;
+        if (currentMonth < month || (currentMonth === month && currentDay < day)) {
+            age--;
+        }
 
-    console.log(`User Age: ${totalYears}`);
+        function validateInput(){
+            var status = true;
+            if(month < 1 || month > 12){
+                alert("Invalid Month. Please enter a valid month between 1 and 12.");
+                status= false;
+            }
+            if(year > currentYear){
+                alert("Invalid Year. You have entered future date.");
+                status=false;
+            }
+            if (month === 2) {
+                const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+                if (isLeapYear && day > 29) {
+                    alert("Invalid day. Please enter day within 1 to 29 range for leap year");
+                    status = false;
+                } else if (!isLeapYear && day > 28) {
+                    alert("Invalid day. Please enter day within 1 to 28 range for non leap year");
+                    status = false;
+                }
+            }
+            if(month==4|| month== 6|| month==9 || month==11 )
+                {
+                    if(day >= 31 ){
+                        alert("Invalid day. Please enter day within 1 to 30 range");
+                        status=false;
+                    }
+                }
+            if( month !=2 && (day < 1 ||  day > 31)){
+                    alert("Invalid day. Please enter day within 1 to 31 range");
+                    status =  false;
+                }
+            return status;
+        }
 
-    if (totalYears < 15) {
-        alert("You are restricted as you are under 15.");
-    } else {
-        window.location.href="https://www.google.com/";
-    }
-}
+        if (!validateInput()) {
+        } 
+        else if (age < 15) {
+            alert("You are restricted as you are under 15.");
+        } 
+        else {
+            window.location.href = "https://www.google.com/";
+            // console.log(`User Age: ${age}`);
+        }
+    });
+});
